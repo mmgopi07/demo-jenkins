f311b379ab56dc6f1f69a35be47bf94ff34fb004
mmgopi07 <mm.gopi92@gmail.com>
2023-03-12 06:49:33 +0530

    first commit

---
+++ demo-app/src/main/java/com/example/DemoAppApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppApplication.class, args);
+		
+		System.out.println("Success....");
+	}
+
+}
